f39c94e9d2d2202b3c35d84cf2e609bf57e7f153
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2023-12-30 09:48:48 +0530

    modified:   package-lock.json 	modified:   package.json 	new file:   src/assets/jpg/rentCategoryImage.jpg 	new file:   src/assets/jpg/sellCategoryImage.jpg 	new file:   src/assets/svg/badgeIcon.svg 	new file:   src/assets/svg/bathtubIcon.svg 	new file:   src/assets/svg/bedIcon.svg 	new file:   src/assets/svg/checkIcon.svg 	new file:   src/assets/svg/deleteIcon.svg 	new file:   src/assets/svg/editIcon.svg 	new file:   src/assets/svg/exploreIcon.svg 	new file:   src/assets/svg/googleIcon.svg 	new file:   src/assets/svg/homeIcon.svg 	new file:   src/assets/svg/keyboardArrowRightIcon.svg 	new file:   src/assets/svg/localOfferIcon.svg 	new file:   src/assets/svg/lockIcon.svg 	new file:   src/assets/svg/personIcon.svg 	new file:   src/assets/svg/personOutlineIcon.svg 	new file:   src/assets/svg/shareIcon.svg     new file:   src/assets/svg/uploadIcon.svg 	new file:   src/assets/svg/visibilityIcon.svg 	new file:   src/firebase.config.js 	modified:   src/index.css

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-ClnvgwjPTnnI6aOPuZtf8q01B5mVvvE",
+  authDomain: "house-marketplace-app-3ad3c.firebaseapp.com",
+  projectId: "house-marketplace-app-3ad3c",
+  storageBucket: "house-marketplace-app-3ad3c.appspot.com",
+  messagingSenderId: "653142047284",
+  appId: "1:653142047284:web:213bbf1836f4c2d1e2e861"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
