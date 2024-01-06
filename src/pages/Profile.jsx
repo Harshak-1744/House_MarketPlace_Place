cc764f9d6d4e0e2b3e5e8c941957b2df42f5bde4
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2024-01-06 08:50:27 +0530

    modified:   src/pages/Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,58 @@
 import {useState, useEffect } from "react"
-import { getAuth } from "firebase/auth"
-
+import { getAuth, updateProfile } from "firebase/auth"
+import { useNavigate,Link } from "react-router-dom"
+import { updateDoc } from 'firebase/firestore'
+import {db} from '../firebase.config'
 
 
 function Profile(){
 
-    const [user, setUser] = useState({})
-
     const auth = getAuth()
-    useEffect(() => {
-        setUser(auth.currentUser)
-    }, [])
+    const {changeDetails, setChangeDetails} = useState(false)
+    const [formData, setFormData] = useState({
+        name: auth.currentUser.displayName,
+        email: auth.currentUser.email  
+      
+    })
 
-    return user ? <h1>{user.displayName}</h1> : 'Not Logged In'
+    const {name,email} = formData
+
+    const navigate = useNavigate()
+
+   const onLogout = () => {
+    auth.signOut()
+    navigate('/')
+   }
+
+   const onSubmit = () => {
+    console.log(123);
+   }
+    return <div className='profile'>
+        
+        <header className="profileHeader">
+            <p className="pageHeader">
+                My Profile
+            </p>
+            <button className="logOut" type= 'button' onClick={onLogout}>
+                Logout
+            </button>
+        </header>
+
+        <main>
+            <div className="profileDetailsHeader">
+                <p className="profileDetailsText">
+                    Personal Details
+                </p>
+                <p className="changePersonalDetails" onClick={() => {
+                    changeDetails && onSubmit()
+                    setChangeDetails((prevState) => !prevState)
+                }}>
+                    {changeDetails ? 'done' : 'change'}
+                </p>
+            </div>
+        </main>
+        
+        </div>
 }
 
 export default Profile
