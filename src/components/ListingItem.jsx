2bcd303f0f9fe60311f227f8a3bb9245582a6f3b
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2024-01-09 10:29:08 +0530

    modified:   src/components/ListingItem.jsx

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -3,11 +3,25 @@ import {ReactComponent as DeleteIcon} from '../assets/svg/deleteIcon.svg'
 import bedIcon from '../assets/svg/bedIcon.svg'
 import bathtubIcon from '../assets/svg/bathtubIcon.svg'
 
-function ListingItem({lisitng, id}){
+function ListingItem({listing, id}){
   return (
     <li className='categoryListing'>
-      <Link to={`/category/${lisitng.type}/${id}`} className='categoryListingLink'>
-        <img src={lisitng.imgUrls[0]} alt={lisitng.name} className='categoryListingImg' />
+      <Link to={`/category/${listing.type}/${id}`} className='categoryListingLink'>
+        <img src={listing.imgUrls[0]} alt={listing.name} className='categoryListingImg' />
+        <div className="categoryListingDe">
+          <p className="categoryListingLocation">
+            {listing.location}
+          </p>
+          <p className="caregoryLisitngName">{listing.name}</p>
+
+          <p className="categoryLisitingPrice">
+            ${listing.offer 
+            ? listing.discountedPrice
+                  .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') 
+            : listing.regularPrice
+                  .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+          </p>
+        </div>
       </Link>
     </li>
   )
