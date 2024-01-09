fa8cecb5bf8f65b4aeac629f0b556d154fc461e9
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2024-01-09 12:00:34 +0530

    modified:   src/components/ListingItem.jsx

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -21,9 +21,20 @@ function ListingItem({listing, id}){
                   .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') 
             : listing.regularPrice
                   .toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-            {listing.type === 'rent'}
+            {listing.type === 'rent' && ' /Month '}
 
           </p>
+
+          <div className="categoryListingInfoDiv">
+              <img src = {bedIcon} alt="bed" />
+              <p className="categoryListingInfoText">
+                {listing.bedrooms  > 1 ? `${listing.bedrooms} Bedrooms` : `1 Bedroom`}
+              </p>
+              <img src={bathtubIcon} alt="bath" />
+              <p className="categoryListingInfoText">
+              {listing.bathrooms  > 1 ? `${listing.bathrooms} Bathrooms` : `1 Bathroom`}
+              </p>
+          </div>
         </div>
       </Link>
     </li>
