7192a53bed87a090ae17986057ebd144e5b971f7
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2024-01-09 08:09:13 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/components/ListingItem.jsx

--- src/components/ListingItem.jsx
+++ src/components/ListingItem.jsx
@@ -1,62 +1,14 @@
-import { Link } from 'react-router-dom'
-import { ReactComponent as DeleteIcon } from '../assets/svg/deleteIcon.svg'
-import { ReactComponent as EditIcon } from '../assets/svg/editIcon.svg'
+import {Link} from 'react-router-dom'
+import {ReactComponent as DeleteIcon} from '../assets/svg/deleteIcon.svg'
 import bedIcon from '../assets/svg/bedIcon.svg'
 import bathtubIcon from '../assets/svg/bathtubIcon.svg'
 
-function ListingItem({ listing, id, onEdit, onDelete }) {
+function ListingItem({lisitng, id}){
   return (
     <li className='categoryListing'>
-      <Link
-        to={`/category/${listing.type}/${id}`}
-        className='categoryListingLink'
-      >
-        <img
-          src={listing.imgUrls[0]}
-          alt={listing.name}
-          className='categoryListingImg'
-        />
-        <div className='categoryListingDetails'>
-          <p className='categoryListingLocation'>{listing.location}</p>
-          <p className='categoryListingName'>{listing.name}</p>
-
-          <p className='categoryListingPrice'>
-            $
-            {listing.offer
-              ? listing.discountedPrice
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-              : listing.regularPrice
-                  .toString()
-                  .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
-            {listing.type === 'rent' && ' / Month'}
-          </p>
-          <div className='categoryListingInfoDiv'>
-            <img src={bedIcon} alt='bed' />
-            <p className='categoryListingInfoText'>
-              {listing.bedrooms > 1
-                ? `${listing.bedrooms} Bedrooms`
-                : '1 Bedroom'}
-            </p>
-            <img src={bathtubIcon} alt='bath' />
-            <p className='categoryListingInfoText'>
-              {listing.bathrooms > 1
-                ? `${listing.bathrooms} Bathrooms`
-                : '1 Bathroom'}
-            </p>
-          </div>
-        </div>
+      <Link to={`/category/${lisitng.type}/${id}`} className='categoryListingLink'>
+        <img src={lisitng.imgUrls[0]} alt={lisitng.name} className='categoryListing' />
       </Link>
-
-      {onDelete && (
-        <DeleteIcon
-          className='removeIcon'
-          fill='rgb(231, 76,60)'
-          onClick={() => onDelete(listing.id, listing.name)}
-        />
-      )}
-
-      {onEdit && <EditIcon className='editIcon' onClick={() => onEdit(id)} />}
     </li>
   )
 }
