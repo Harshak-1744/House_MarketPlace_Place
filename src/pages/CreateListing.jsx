7a14b7e6222d3b45bdbc348fae8baaaa71ad6492
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2024-01-11 20:59:55 +0530

    modified:   .gitignore 	modified:   src/pages/CreateListing.jsx

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -92,7 +92,7 @@ function CreateListing() {
 
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=$AIzaSyAcIkV8scrP6vvOOM8FI1foydwTyeUPP6I`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
       )
 
       const data = await response.json()
