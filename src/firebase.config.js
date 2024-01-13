821d6c67592a75f51b4802778f5173e27c4ac83b
Harsha <85007461+Harshak-1744@users.noreply.github.com>
2024-01-13 23:03:15 +0530

    new file:   .gitignore 	new file:   README.md 	new file:   package-lock.json 	new file:   package.json 	new file:   public/favicon.ico 	new file:   public/index.html 	new file:   public/logo192.png 	new file:   public/logo512.png 	new file:   public/manifest.json 	new file:   public/robots.txt 	new file:   src/App.js 	new file:   src/assets/jpg/rentCategoryImage.jpg 	new file:   src/assets/jpg/sellCategoryImage.jpg 	new file:   src/assets/svg/badgeIcon.svg 	new file:   src/assets/svg/bathtubIcon.svg 	new file:   src/assets/svg/bedIcon.svg 	new file:   src/assets/svg/checkIcon.svg 	new file:   src/assets/svg/deleteIcon.svg 	new file:   src/assets/svg/editIcon.svg 	new file:   src/assets/svg/exploreIcon.svg 	new file:   src/assets/svg/googleIcon.svg 	new file:   src/assets/svg/homeIcon.svg 	new file:   src/assets/svg/keyboardArrowRightIcon.svg 	new file:   src/assets/svg/localOfferIcon.svg 	new file:   src/assets/svg/lockIcon.svg 	new file:   src/assets/svg/personIcon.svg 	new file:   src/assets/svg/personOutlineIcon.svg 	new file:   src/assets/svg/shareIcon.svg 	new file:   src/assets/svg/uploadIcon.svg 	new file:   src/assets/svg/visibilityIcon.svg 	new file:   src/components/ListingItem.jsx 	new file:   src/components/Navbar.jsx 	new file:   src/components/OAuth.jsx 	new file:   src/components/PrivateRoute.jsx 	new file:   src/components/Slider.jsx 	new file:   src/components/Spinner.jsx 	modified:   src/firebase.config.js 	new file:   src/hooks/useAuthStatus.js 	new file:   src/index.css 	new file:   src/index.js 	new file:   src/pages/Category.jsx 	new file:   src/pages/Contact.jsx 	new file:   src/pages/CreateListing.jsx 	new file:   src/pages/EditListing.jsx 	new file:   src/pages/Explore.jsx 	new file:   src/pages/ForgotPassword.jsx 	new file:   src/pages/Listing.jsx 	new file:   src/pages/Offers.jsx 	new file:   src/pages/Profile.jsx 	new file:   src/pages/SignIn.jsx 	new file:   src/pages/SignUp.jsx 	new file:   src/reportWebVitals.js

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -1,15 +1,16 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app'
+import { getFirestore } from 'firebase/firestore'
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA-ClnvgwjPTnnI6aOPuZtf8q01B5mVvvE",
-  authDomain: "house-marketplace-app-3ad3c.firebaseapp.com",
-  projectId: "house-marketplace-app-3ad3c",
-  storageBucket: "house-marketplace-app-3ad3c.appspot.com",
-  messagingSenderId: "653142047284",
-  appId: "1:653142047284:web:213bbf1836f4c2d1e2e861"
-};
+  apiKey: 'AIzaSyDA8LVcBB6ZuFMGtZZLEh_veJ44WGrNRdE',
+  authDomain: 'house-marketplace-app-fb1d0.firebaseapp.com',
+  projectId: 'house-marketplace-app-fb1d0',
+  storageBucket: 'house-marketplace-app-fb1d0.appspot.com',
+  messagingSenderId: '832068369979',
+  appId: '1:832068369979:web:dce177da9bfc60a4b4e61e',
+}
 
 // Initialize Firebase
-initializeApp(firebaseConfig);
+initializeApp(firebaseConfig)
 export const db = getFirestore()
